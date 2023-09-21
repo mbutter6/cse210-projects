d58d16bdd601f2a6c54c45b130b4ca0d756639d0
Michelle ButterfieldE <mbutter6@byui.edu>
2023-09-20 19:48:40 -0600

    prep1

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,49 @@
 using System;
 
-class Program
+class Program1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess?");
+        // if (magicNumber > 5){
+        // Console.WriteLine('greater');
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
